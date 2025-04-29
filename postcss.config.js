d1cec27c172f73eaccfcd1a1a4b3457f5961105d
mxsafiri <v.muhagachi@gmail.com>
2025-04-29 15:37:37 +0300

    Fix Tailwind CSS integration with Vite

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,8 @@
  */
 export default {
   plugins: {
+    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
-  },
+  }
 }
--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     emptyOutDir: true,
   },
   css: {
-    postcss: './postcss.config.js',
+    devSourcemap: true,
   },
   resolve: {
     alias: {
