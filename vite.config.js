8a0ad5365251ff0bd053bb90b7b63a535d89e76f
mxsafiri <v.muhagachi@gmail.com>
2025-04-29 15:11:04 +0300

    Add Netlify deployment configuration

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: '.wasp/build/web',
+    rollupOptions: {
+      input: 'index.html'
+    }
+  },
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, './src')
+    }
+  }
+});
